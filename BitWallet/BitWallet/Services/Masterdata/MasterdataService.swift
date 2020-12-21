b60d173868b8604e65e82993aee7417480c23946
Dmitrii Borodko <90deem@yandex-team.ru>
2020-12-22 00:39:37 +0300

    configured MasterdataProvider

--- BitWallet/BitWallet/Services/Masterdata/MasterdataService.swift
+++ BitWallet/BitWallet/Services/Masterdata/MasterdataService.swift
@@ -10,17 +10,44 @@ class DefaultMasterdataService: MasterdataService {
 
     // MARK: - Interface
 
+    enum Error: Swift.Error {
+        case emptyMasterdata
+        case decoding
+    }
+
     init(masterdataDataStore: MasterdataDataStore = Services.masterdataDataStore) {
         self.masterdataDataStore = masterdataDataStore
     }
 
     func fetchMasterdata() -> Promise<Masterdata> {
-        return Promise.value(Masterdata())
+        return Promise { seal in
+            DispatchQueue.main.async { [unowned self] in
+                if let masterdata = self.masterdataDataStore.restoreMasterdata() {
+                    seal.fulfill(masterdata)
+                    return
+                }
+
+                guard let path = Bundle.main.path(forResource: "Mastrerdata", ofType: "json") else {
+                    seal.reject(Error.emptyMasterdata)
+                    return
+                }
+
+                do {
+                    let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+                    let masterdataDTO = try JSONDecoder().decode(MasterdataDTO.self, from: data)
+                    let masterdata = Masterdata(withDTO: masterdataDTO)
+
+                    self.masterdataDataStore.storeMasterdata(masterdata)
+
+                    seal.fulfill(masterdata)
+                } catch let error {
+                    seal.reject(error)
+                }
+            }
+        }
     }
 
     // MARK: - Implementation
 
     private let masterdataDataStore: MasterdataDataStore
-
-
 }
